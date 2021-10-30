292d042a387b43c4eaad6b6aba5f439050eb14b6
Haha007 <nichtechtanonym@gmail.com>
2021-10-30 17:16:17 +0200

    SneakyThrows -> try catch

--- src/main/java/at/emielregis/worldeditanimations/AnimationFrame.java
+++ src/main/java/at/emielregis/worldeditanimations/AnimationFrame.java
@@ -2,6 +2,7 @@ package at.emielregis.worldeditanimations;
 
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
@@ -11,7 +12,6 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.world.World;
-import lombok.SneakyThrows;
 import org.bukkit.Location;
 import org.bukkit.Material;
 
@@ -24,27 +24,29 @@ public class AnimationFrame {
         clipboard = clip;
     }
 
-    @SneakyThrows
     public void paste() {
-        EditSession session = WorldEdit.getInstance().newEditSession(adapt(location.getWorld()));
-        Operation operation = new ClipboardHolder(clipboard)
-                .createPaste(session)
-                .to(BlockVector3.at(location.getX(), location.getY(), location.getZ()))
-                .build();
-        Operations.complete(operation);
-        session.close();
+        try (EditSession session = WorldEdit.getInstance().newEditSession(adapt(location.getWorld()))) {
+            Operation operation = new ClipboardHolder(clipboard)
+                    .createPaste(session)
+                    .to(BlockVector3.at(location.getX(), location.getY(), location.getZ()))
+                    .build();
+            Operations.complete(operation);
+        } catch (WorldEditException e) {
+            e.printStackTrace();
+        }
     }
 
-    @SneakyThrows
     public void delete() {
-        EditSession session = WorldEdit.getInstance().newEditSession(adapt(location.getWorld()));
-        BlockVector3 difference = clipboard.getOrigin().subtract(clipboard.getMinimumPoint());
-        Region region = clipboard.getRegion().clone();
-        region.shift(region.getMinimumPoint().multiply(-1));
-        region.shift(BlockVector3.at(location.getBlockX(), location.getBlockY(), location.getBlockZ()));
-        region.shift(difference.multiply(-1));
-        session.setBlocks(region, BukkitAdapter.adapt(Material.AIR.createBlockData()));
-        session.close();
+        try (EditSession session = WorldEdit.getInstance().newEditSession(adapt(location.getWorld()))) {
+            BlockVector3 difference = clipboard.getOrigin().subtract(clipboard.getMinimumPoint());
+            Region region = clipboard.getRegion().clone();
+            region.shift(region.getMinimumPoint().multiply(-1));
+            region.shift(BlockVector3.at(location.getBlockX(), location.getBlockY(), location.getBlockZ()));
+            region.shift(difference.multiply(-1));
+            session.setBlocks(region, BukkitAdapter.adapt(Material.AIR.createBlockData()));
+        } catch (WorldEditException e) {
+            e.printStackTrace();
+        }
     }
 
     public static World adapt(org.bukkit.World world) {
